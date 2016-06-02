eb198f19a3fc6dd89d31eccc1ce8c490676a097c
张海亮 <zhang.hl@Ctrip.com>
2016-06-02 20:50:33 +0800

    update exe to release

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ define(function() {
     var config = {
     	Version : window.ServiceVersion,
     	websocketUrl : "ws://127.0.0.1:8181/",
-    	ServerPakage :window.location.origin + window.location.pathname + 'server/Calibur/bin/Debug/Calibur.exe'
+    	ServerPakage :window.location.origin + window.location.pathname + 'server/Calibur/bin/Release/Calibur.exe'
     };
     return config;
 });
